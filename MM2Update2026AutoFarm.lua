0770fe976eb8ee7234dd70a7dd5bf37f7c32ee00
AAAOMGUEU <soprojohnnyj@gmail.com>
2026-08-03 20:30:39 -0500

    Update script URL for AutoFarm functionality

--- MM2Update2026AutoFarm.lua
+++ MM2Update2026AutoFarm.lua
@@ -1,2 +1,2 @@
 -- hj
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AAAOMGUEU/mm2opscripts/refs/heads/main/MM2AutoFarmSummerUpdate.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AAAOMGUEU/mm2/refs/heads/main/AutoFarmNewVersion.lua"))()
